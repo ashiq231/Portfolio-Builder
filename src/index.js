bb9ec806f85823cf28f1de26f219cb79e29be623
Md. Jubayer Hossen <mdjubayerhossen969@gmail.com>
2026-02-09 03:18:14 +0600

    Update index.js
    updated intex,js  file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './styles/themes.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
